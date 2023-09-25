a901e3f5f9a5199aa4d74857879caf2791a39c89
ReDay Zarra <redayzarra@gmail.com>
2023-09-24 20:24:36 -0400

    I need to think about what the customer needs

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -3,19 +3,9 @@ import SubmitButton from "./SubmitButton";
 
 const DemoPage = () => {
   return (
-    <form onSubmit={() => null}>
-      <div className="mb-3">
-        <label htmlFor="IG_Username" className="form-label">
-          Instagram Username:{" "}
-        </label>
-        <input
-          id="IG_Username"
-          type="text"
-          className="input input-bordered w-full max-w-xs mt-1"
-        />
-      </div>
-      <SubmitButton />
-    </form>
+    <div>
+      Demo Page
+    </div>
   );
 };
 
