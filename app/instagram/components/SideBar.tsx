6d6d5c59445b91c506bcaf81bde86b3a1aec9001
ReDay Zarra <redayzarra@gmail.com>
2023-09-27 12:59:08 -0400

    Learning basic tailwind css styles

--- app/instagram/components/SideBar.tsx
+++ app/instagram/components/SideBar.tsx
@@ -6,6 +6,10 @@ const SideBar = () => {
   return (
     <div>
       <Avatar />
+      <div className="flex flex-col items-center justify-center mt-4">
+        <h1 className="text-2xl font-semibold">ReDay</h1>
+        <h2 className="text-sm text-gray-500">Welcome to my Instagram</h2>
+      </div>
     </div>
   );
 };
