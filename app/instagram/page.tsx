0a342e191da25cb50101643b5f3a9b3a35341a1f
ReDay Zarra <redayzarra@gmail.com>
2023-10-01 14:29:36 -0400

    Starting to test grid component using JoyUI

--- app/instagram/page.tsx
+++ app/instagram/page.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import * as React from "react";
+import Grid from "@mui/joy/Grid";
+import Item from "./components/Item";
 
 const Instagram = () => {
   return (
     <div>
-      
+      <Grid container spacing={2} sx={{ flexGrow: 1 }}>
+        <Grid xs={0} md={1}>
+          <Item>xs=6 md=8</Item>
+        </Grid>
+        <Grid xs={6} md={4}>
+          <Item>xs=6 md=4</Item>
+        </Grid>
+        <Grid xs={6} md={4}>
+          <Item>xs=6 md=4</Item>
+        </Grid>
+        <Grid xs={6} md={8}>
+          <Item>xs=6 md=8</Item>
+        </Grid>
+      </Grid>
     </div>
-  )
-}
+  );
+};
 
-export default Instagram
+export default Instagram;
