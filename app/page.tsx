5df65ff5e9d95c47dfdbc1e4fa6f0aa25e9ef360
ReDay Zarra <redayzarra@gmail.com>
2023-10-01 14:14:54 -0400

    Adding integration with JoyUI

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <h1>Hello World!</h1>
-      <Link href="/instagram">Instagram</Link>
+      <Link href="/instagram" className="font-bold">
+        Instagram
+      </Link>
     </div>
   );
 }
