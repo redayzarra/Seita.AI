5648fbb8ab34fd173f999be916ba941f3ed90143
ReDay Zarra <redayzarra@gmail.com>
2023-10-06 16:07:14 -0400

    Removing side links and planning next steps

--- app/instagram/components/sidebar/BarStack.tsx
+++ app/instagram/components/sidebar/BarStack.tsx
@@ -1,11 +1,8 @@
 import { Stack } from "@mui/joy";
-import { Insights } from "../Icons";
-import ClientSideLink from "./sidelinks/ClientSideLink";
 
 const BarStack = () => {
   return (
     <Stack spacing={2}>
-      <ClientSideLink href="insights" icon={<Insights />} title="Insights" />
     </Stack>
   );
 };
